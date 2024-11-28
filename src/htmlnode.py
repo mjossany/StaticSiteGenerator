e92718fe3d4e7c3daf4c47300aea6904748d5469
Jossany Moura <jossany.junior@e-unicred.com.br>
2024-11-28 20:15:31 -0300

    implement LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,7 +13,6 @@ class HTMLNode():
     def props_to_html(self):
         list_key_value = self.props.items()
         final_string = reduce(lambda accumulator, current_tuple: accumulator + f' {current_tuple[0]}="{current_tuple[1]}"', list_key_value, "")
-        print(f'FINAL STRING: {final_string}')
         return final_string
 
     def __eq__(self, other):
@@ -29,8 +28,6 @@ class HTMLNode():
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        if value is None:
-            raise ValueError("LeafNode requires a non-None value.")
         super().__init__(tag=tag, value=value, children=None, props=props)
     
     def to_html(self):
@@ -38,5 +35,17 @@ class LeafNode(HTMLNode):
             raise ValueError("LeafNode requires a non-None value.")
         if self.tag is None:
             return self.value
-        
+        if self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag and
+            self.value == other.value and
+            self.children == other.children and
+            self.props == other.props
+        )
+        
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.children}, {self.props})'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -17,3 +17,32 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "hello world!", None, {'href': 'https://www.google.com', 'target': '_blank',})
         node2 = HTMLNode("p", "hello world!", None, {'href': 'https://www.google.com', 'target': '_blank',})
         self.assertEqual(node, node2)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        node_html_tag = node.to_html()
+        self.assertEqual(node_html_tag, "<p>This is a paragraph of text.</p>")
+    
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node_html_tag = node.to_html()
+        self.assertEqual(node_html_tag, '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_node_without_value(self):
+        node = LeafNode("a", None, {"href": "https://www.google.com"})
+        self.assertRaises(ValueError, node.to_html)
+    
+    def test_leaf_node_without_tag(self):
+        node = LeafNode(None, "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "Click me!")
+
+    def test_eq(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node_repr = node.__repr__()
+        self.assertEqual(node_repr, "LeafNode(a, Click me!, None, {'href': 'https://www.google.com'})")
